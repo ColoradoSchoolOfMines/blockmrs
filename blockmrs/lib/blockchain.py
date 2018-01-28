a9c6c4e8f4861099d5b1c9e27d93717caf86755b
David Florness <edwargix@gmail.com>
2018-01-28 07:34:27 -0700

    delete trailing whitespace

--- blockmrs/lib/blockchain.py
+++ blockmrs/lib/blockchain.py
@@ -14,19 +14,19 @@ class Blockchain:
             chain_file.write(user_sig)
             chain_file.write(user_public_key)
             chain_file.write(recipient_public_key)
-        
+
         with open(self.file_name, 'rb') as chain_file:
             return int(os.path.getsize('/tmp/bullshit.dat'))//1658 - 1
 
 
-        
+
 
     def lookup_blockchain_entry(self, index):
-        
+
         with open(self.file_name, 'rb') as chain_file:
             seek_index = index * 1658
             chain_file.seek(seek_index)
-            
+
             ipfs_hash = chain_file.read(46)
             user_sig = chain_file.read(512)
             user_public_key = chain_file.read(550)
