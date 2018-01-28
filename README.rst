deddb367b6836218e2f017db838afbb82354ec09
Sumner Evans <sumner.evans98@gmail.com>
2018-01-28 01:21:05 -0600

    Update homepage, add signup/login page

--- README.rst
+++ README.rst
@@ -3,4 +3,10 @@ BlockMRS
 Sports Medical Records on a Blockchain
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
+BlockMRS is a privacy concious, decentralized, blockchain-backed, medical
+record system for the 21st Century.
+
+It stores patient records in IPFS using state-of-the art asymetric encryption
+giving patients control over their health data.
+
 TODO: Somebody write a README
--- blockmrs/controllers/root.py
+++ blockmrs/controllers/root.py
@@ -38,7 +38,7 @@ class RootController(BaseController):
         return dict(page='editor stuff')
 
     @expose('blockmrs.templates.login')
-    def login(self, came_from=lurl('/'), failure=None, login=''):
+    def login(self, came_from=lurl('/'), failure=None, login='', signup=False):
         """Start the user login."""
         if failure is not None:
             if failure == 'user-not-found':
@@ -50,8 +50,11 @@ class RootController(BaseController):
         if failure is None and login_counter > 0:
             flash(_('Wrong credentials'), 'warning')
 
-        return dict(page='login', login_counter=str(login_counter),
-                    came_from=came_from, login=login)
+        return dict(page='login',
+                    login_counter=str(login_counter),
+                    came_from=came_from,
+                    signup=signup,
+                    login=login)
 
     @expose()
     def post_login(self, came_from=lurl('/')):
