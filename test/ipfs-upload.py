2af2b019d414b6c36e121a5eeb839e8053513fe4
Robby Zampino <robozman@users.noreply.github.com>
2018-01-28 05:00:58 -0500

    ipfs-upload.py: save file to ipfs and create bytes to save to 'blockchain'

---
+++ test/ipfs-upload.py
@@ -0,0 +1,44 @@
+import ipfsapi
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+
+with open("public_key.pub", "rb") as key_file:
+    public_key = serialization.load_pem_public_key(
+        key_file.read(),
+        backend=default_backend()
+    )
+
+with open("private_key.pem", "rb") as key_file:
+    private_key = serialization.load_pem_private_key(
+        key_file.read(),
+        password=b'test',
+        backend=default_backend()
+    )
+
+api = ipfsapi.connect('127.0.0.1', 5001)
+ipfs_file_handle = api.add('dummy.csv')
+ipfs_hash = ipfs_file_handle['Hash']
+ipfs_hash_bytes = ipfs_hash.encode(encoding='UTF-8')
+
+hash_signature = private_key.sign(
+    ipfs_hash_bytes,
+    padding.PSS(
+        mgf=padding.MGF1(hashes.SHA256()),
+        salt_length=padding.PSS.MAX_LENGTH
+    ),
+    hashes.SHA256()
+)
+
+
+public_key_bytes = public_key.public_bytes(encoding=serialization.Encoding.DER, format=serialization.PublicFormat.SubjectPublicKeyInfo)
+
+print("ipfs_hash_bytes:")
+print(ipfs_hash_bytes)
+print("hash_signature:")
+print(hash_signature)
+print("public_key_bytes")
+print(public_key_bytes)
+
